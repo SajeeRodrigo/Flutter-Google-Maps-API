baea701c22dbed8a941d306abaa0e029c7fdde2b
SajeeRodrigo <sajee.thamanga@gmail.com>
2022-05-27 16:07:48 +0530

    .env add

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_api/screens/google_map_screen.dart';
+import 'package:flutter_config/flutter_config.dart';
 
-
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
+
+  await FlutterConfig.loadEnvVariables();
 }
 
 class MyApp extends StatelessWidget {
@@ -13,6 +16,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    print(FlutterConfig.get('API_KEY'));
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -31,7 +35,7 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Home Screen'),
       ),
-       body: Center(
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
@@ -42,20 +46,19 @@ class HomePage extends StatelessWidget {
                 fontWeight: FontWeight.bold,
               ),
             ),
-            
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => Navigator.push(
-          context, MaterialPageRoute(
+          context,
+          MaterialPageRoute(
             builder: (context) => GoogleMapScreen(),
-            ),
-            ),
+          ),
+        ),
         tooltip: 'Google Map',
         child: const Icon(Icons.pin_drop_outlined),
       ),
     );
   }
 }
-
