88184c96b915ddadd3c61b1b9cdf716ba7439d24
SajeeRodrigo <sajee.thamanga@gmail.com>
2022-05-27 11:40:44 +0530

    create map first step

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_maps_api/screens/google_map_screen.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.lightGreen,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+      ),
+       body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Google Map Demo',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => Navigator.push(
+          context, MaterialPageRoute(
+            builder: (context) => GoogleMapScreen(),
+            ),
+            ),
+        tooltip: 'Google Map',
+        child: const Icon(Icons.pin_drop_outlined),
+      ),
+    );
+  }
+}
+
---
+++ lib/screens/google_map_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+
+class GoogleMapScreen extends StatefulWidget {
+  @override
+  State<GoogleMapScreen> createState() => _GoogleMapScreenState();
+}
+
+class _GoogleMapScreenState extends State<GoogleMapScreen> {
+  Set<Marker> _markers = {};
+
+
+
+
+  void _onMapCreated(GoogleMapController controller) {
+    setState(() {
+      _markers.add(
+        Marker(
+          markerId: MarkerId('id-1'),
+          position: LatLng(22.5448131, 88.3403691),
+       
+          infoWindow: InfoWindow(
+            title: 'Victoria Memorial',
+            snippet: 'A Historical Place',
+          )
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Google Map'),
+      ),
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        markers: _markers,
+        initialCameraPosition: CameraPosition(
+          target: LatLng(22.5448131, 88.3403691),
+          zoom: 15,
+        ),
+      ),
+    );
+  }
+}
